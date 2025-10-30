25f7cb15a619403ff705cd3c38a3f894243d728a
Imanol-Cervantes <imanolplays@icloud.com>
2025-10-29 18:25:02 -0600

    Add files via upload

---
+++ ProgramOne.py
@@ -0,0 +1,15 @@
+# Name : Imanol Cervantes
+# Assignment: Program 1 (+2)
+
+# Step 1
+print( "  @..@ ")
+print(" (----)")
+print("( >__< )")
+print("^^ ~~ ^^")
+
+
+# Step 2
+state = input("US State: ")
+capital = input("Capital: ")
+print("The capital of " + state + " is " + capital + ".")
+
